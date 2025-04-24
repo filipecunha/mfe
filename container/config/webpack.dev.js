04008ca58e0397fc8a97274b1c22759fb2a4a91d
Filipe Cunha <filipe.cunha@nstech.com.br>
2025-04-24 20:09:34 -0300

    Initial commit

---
+++ container/config/webpack.dev.js
@@ -0,0 +1,34 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common.js');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
+
+const devConfig = {
+	mode: 'development',
+	devtool: 'eval-source-map',
+	devServer: {
+		port: 8080,
+		historyApiFallback: {
+			index: '/index.html',
+		},
+		hot: true,
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+			name: 'container',
+			remotes: {
+				marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+			},
+			shared: packageJson.dependencies,
+		}),
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+	],
+	output: {
+		publicPath: 'auto',
+	},
+};
+
+module.exports = merge(commonConfig, devConfig);
